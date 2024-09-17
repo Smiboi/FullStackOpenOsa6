ac4ccd2f7f5528ec7a17264906551ce80e9f206c
Smiboi <128560300+Smiboi@users.noreply.github.com>
2024-09-18 02:20:36 +0300

    Yritin tehdä tehtävää 6.13

--- redux-anecdotes/src/reducers/notificationReducer.js
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -8,9 +8,36 @@ const notificationSlice = createSlice({
   reducers: {
     setNotification(state, action) {
       return action.payload
+    },
+/* 
+    clearNotification(state, action) {
+      return ''
     }
+ */
   }
 })
+/* 
+export const addNotification = (setNotification) => {
+  return {
+    type: 'notification/setNotification',
+    payload: setNotification
+  }
+}
 
-export const { setNotification } = notificationSlice.actions
+export const removeNotification = () => {
+  return {
+    type: 'notification/setNotification',
+    payload: ''
+  }
+}
+ */
+/* 
+export const showNotification = (message, time) => {
+  return async dispatch => {
+    setTimeout(() => { dispatch(clearNotification()) }, time * 1000)
+    dispatch(setNotification(message))
+  }
+}
+ */
+export const { setNotification/* , clearNotification */ } = notificationSlice.actions
 export default notificationSlice.reducer
