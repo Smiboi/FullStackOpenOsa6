0b2cbef98e72b8fd34d785144e31d6ded40d46d0
Smiboi <128560300+Smiboi@users.noreply.github.com>
2024-09-18 23:41:48 +0300

    Tehtävä 6.18 tehty

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -26,22 +26,32 @@ const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
+/* 
+    createAnecdote(state, action) {
+      state.push(action.payload)
+    },
+ */
+/* 
     addVote(state, action) {
       const id = action.payload
       const anecdoteToVote = state.find(a => a.id === id)
       const votedAnecdote = { ...anecdoteToVote, votes: anecdoteToVote.votes + 1 }
       return state.map(anecdote => anecdote.id !== id ? anecdote : votedAnecdote)
     },
+ */
     appendAnecdote(state, action) {
       state.push(action.payload)
     },
     setAnecdotes(state, action) {
       return action.payload
+    },
+    updateAnecdote(state, action) {
+      return state.map(anecdote => anecdote.id !== action.payload.id ? anecdote : action.payload)
     }
   }
 })
 
-export const { addVote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+export const { appendAnecdote, setAnecdotes, updateAnecdote } = anecdoteSlice.actions
 
 export const initializeAnecdotes = () => {
   return async dispatch => {
@@ -54,6 +64,15 @@ export const createAnecdote = content => {
   return async dispatch => {
     const newAnecdote = await anecdoteService.createNew(content)
     dispatch(appendAnecdote(newAnecdote))
+  }
+}
+
+export const addVote = id => {
+  return async (dispatch, getState) => {
+    const anecdoteToVote = getState().anecdotes.find(a => a.id === id)
+    const votedAnecdote = { ...anecdoteToVote, votes: anecdoteToVote.votes + 1 }
+    const updatedAnecdote = await anecdoteService.update(id, votedAnecdote)
+    dispatch(updateAnecdote(updatedAnecdote))
   }
 }
 
--- redux-anecdotes/src/services/anecdotes.js
+++ redux-anecdotes/src/services/anecdotes.js
@@ -13,7 +13,13 @@ const createNew = async (content) => {
   return response.data
 }
 
+const update = async (id, object) => {
+  const response = await axios.put(baseUrl + '/' + id, object)
+  return response.data
+}
+
 export default {
   getAll,
   createNew,
+  update
 }
