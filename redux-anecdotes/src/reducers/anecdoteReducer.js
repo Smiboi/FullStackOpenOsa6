cf222cb85a724613a7c77dd1f364e2f731f23620
Smiboi <128560300+Smiboi@users.noreply.github.com>
2024-09-18 20:55:22 +0300

    Tehtävä 6.15 tehty

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -26,12 +26,7 @@ const anecdoteSlice = createSlice({
   initialState: [],
   reducers: {
     createAnecdote(state, action) {
-      const content = action.payload
-      state.push({
-        content,
-        id: getId(),
-        votes: 0
-      })
+      state.push(action.payload)
     },
     addVote(state, action) {
       const id = action.payload
@@ -41,9 +36,12 @@ const anecdoteSlice = createSlice({
     },
     appendAnecdote(state, action) {
       state.push(action.payload)
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { createAnecdote, addVote, appendAnecdote } = anecdoteSlice.actions
+export const { createAnecdote, addVote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
--- redux-anecdotes/src/store.js
+++ redux-anecdotes/src/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
 
 import anecdoteService from './services/anecdotes'
-import anecdoteReducer, { appendAnecdote } from './reducers/anecdoteReducer'
+import anecdoteReducer, { setAnecdotes } from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
 
@@ -15,9 +15,7 @@ const reducer = combineReducers({
 const store = configureStore({ reducer })
 
 anecdoteService.getAll().then(anecdotes =>
-  anecdotes.forEach(anecdote => {
-    store.dispatch(appendAnecdote(anecdote))
-  })
+  store.dispatch(setAnecdotes(anecdotes))
 )
 
 console.log(store.getState())
